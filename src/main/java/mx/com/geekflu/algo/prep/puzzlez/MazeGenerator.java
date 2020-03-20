a5c345990202eb61d7f0cb8f5d2ab69d80c71407
Luis Gonzalez <Luisenrique.Gonzalezhernandez@walmartlabs.com>
2020-03-20 08:21:39 -0700

    Maze generator for Intellij Academy

---
+++ src/main/java/mx/com/geekflu/algo/prep/puzzlez/MazeGenerator.java
@@ -0,0 +1,55 @@
+package mx.com.geekflu.algo.prep.puzzlez;
+
+import java.util.Random;
+
+/**
+ * Implement the maze as a two-dimensional array of integers. 
+ * If the value of an element is 1, it is a wall. If the value is 0, it is a pass.
+ * 1. There should be walls around the maze, with the exception of two cells - entrance and exit.
+ * 
+ * 2. Any empty cell must be accessible from the entrance or exit of the maze. 
+ * It is not possible to walk along the maze diagonally, only vertically and horizontally.
+ * 
+ * 3. There's got to be a path from the entrance to the exit. 
+ * It doesn't matter what is considered an entrance and what is an exit as they are interchangeable.
+ * 
+ * To print the array, follow these two rules:
+ * to display a pass, use two space characters
+ * to display a wall, use two block characters in a row (the same): █ (U+2588). 
+ * You can print the wall like this: System.out.print("\u2588\u2588").
+ * Remember that the maze is only visible if you have a monospace font! Otherwise, 
+ * the space symbol will be quite small in width. 
+ * At this stage, it does not matter which maze you display. 
+ * The program may always output the same prepared maze or one of a set of prepared mazes.
+ * @author vn04q89
+ *
+ */
+public class MazeGenerator {
+	
+	private static final String PASS = "  ";
+	
+	private static int[][] maze = {
+			{1,0,1,1,1,1,1,1,1},
+			{1,0,1,0,0,0,0,1,1},
+			{1,0,0,1,1,1,0,0,1},
+			{1,1,0,1,0,0,0,0,1},
+			{1,1,0,1,0,1,1,0,1},
+			{1,0,0,0,0,1,1,0,1},
+			{1,1,0,1,1,1,1,0,1},
+			{1,0,0,0,0,0,0,0,0},
+			{1,1,1,1,1,1,1,1,1},
+	};
+
+	public static void main(String[] args) {
+		for(int i = 0 ; i < maze.length ; i++) {
+			for(int j = 0 ; j < maze.length ; j++) {
+				if(maze[i][j] == 0) {
+					System.out.print("  ");
+				}else if(maze[i][j] == 1) {
+					System.out.print("\u2588\u2588");
+				}
+			}
+			System.out.println("");
+		}
+    }
+}
