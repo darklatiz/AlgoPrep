b3f035dca7b98c31351016c307358507f7d2ee75
Spike Storm <software.j2ee@gmail.com>
2020-07-04 12:41:21 -0700

    primer problema con arreglos.

--- src/main/java/mx/com/geekflu/algo/prep/data/arrays/Problems.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/Problems.java
@@ -3,8 +3,20 @@ package mx.com.geekflu.algo.prep.data.arrays;
 public class Problems {
 
   public int findMaxConsecutiveOnes(int[] nums) {
-
-    return 0;
+    if(nums == null || nums.length <=0) return 0;
+    int onecounter = 0;
+    int maxCounter = 0;
+    for (int i = 0; i < nums.length; i++){
+      if(nums[i] == 1){
+        onecounter++;
+        if(maxCounter < onecounter){
+          maxCounter = onecounter;
+        }
+      }else{
+        onecounter = 0;
+      }
+    }
+    return maxCounter;
   }
 
   public static void main(String[] args) {
@@ -14,6 +26,7 @@ public class Problems {
 
   private void executeFindMaxConsecutiveOnes() {
     int[] nums = {1,1,0,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1};
+    int[] nums1 = {1,0,1,1,0,1};
     System.out.println(findMaxConsecutiveOnes(nums));
   }
 
