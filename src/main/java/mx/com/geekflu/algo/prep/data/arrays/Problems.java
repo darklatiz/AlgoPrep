0495463052b4182ad81905230bd8a2b13f556e00
Spike Storm <software.j2ee@gmail.com>
2020-07-04 19:34:53 -0700

    primer problema con arreglos.

--- src/main/java/mx/com/geekflu/algo/prep/data/arrays/Problems.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/Problems.java
@@ -1,20 +1,21 @@
 package mx.com.geekflu.algo.prep.data.arrays;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 public class Problems {
 
   public int findMaxConsecutiveOnes(int[] nums) {
-    if(nums == null || nums.length <=0) return 0;
+    if (nums == null || nums.length <= 0) return 0;
     int onecounter = 0;
     int maxCounter = 0;
-    for (int i = 0; i < nums.length; i++){
-      if(nums[i] == 1){
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 1) {
         onecounter++;
-        if(maxCounter < onecounter){
+        if (maxCounter < onecounter) {
           maxCounter = onecounter;
         }
-      }else{
+      } else {
         onecounter = 0;
       }
     }
@@ -23,8 +24,8 @@ public class Problems {
 
   public int findNumbers(int[] nums) {
     int counter = 0;
-    for(int i = 0; i < nums.length; i++){
-      if(nums[i] >= 10 && nums[i] < 100) counter++;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] >= 10 && nums[i] < 100) counter++;
       else if (nums[i] >= 1000 && nums[i] < 10000) counter++;
       else if (nums[i] == 100000) counter++;
     }
@@ -32,11 +33,49 @@ public class Problems {
   }
 
   public int[] sortedSquares(int[] A) {
-    for (int i = 0; i < A.length; i++){
+    for (int i = 0; i < A.length; i++) {
       A[i] = A[i] * A[i];
     }
     Arrays.sort(A);
     return A;
+  }
+
+  public int[] sortedSquaresAlternateSolution(int[] A) {
+    int negIdx = -1;
+    int posIdx = A.length;
+    for (int i = 0; i < A.length; i++) {
+      if (A[i] < 0) negIdx = i;
+      else if (A[i] >= 0) {
+        posIdx = i;
+        break;
+      }
+    }
+    int[] B = new int[A.length];
+    int Bidx = 0;
+
+    while (negIdx >= 0 && posIdx < A.length) {
+      int negSquare = A[negIdx] * A[negIdx];
+      int posSquare = A[posIdx] * A[posIdx];
+      if(negSquare < posSquare){
+        B[Bidx++] = negSquare;
+        negIdx--;
+      }else{
+        B[Bidx++] = posSquare;
+        posIdx++;
+      }
+    }
+
+    while (A.length >= 1 && negIdx >= 0){
+      B[Bidx++] = A[negIdx] * A[negIdx];
+      negIdx--;
+    }
+
+    while(A.length >= 1 && posIdx < A.length){
+      B[Bidx++] = A[posIdx] * A[posIdx];
+      posIdx++;
+    }
+
+    return B;
   }
 
   public static void main(String[] args) {
@@ -47,18 +86,22 @@ public class Problems {
   }
 
   private void executeSortedSquares() {
-    int[] B = {-4,-1,0,3,10};
-    System.out.println(sortedSquares(B));
+    int[] B = {-4, -1, 0, 3, 10};
+    int[] B1 = {-234,-123, -100, -33, -11, -4, -1, 1, 3, 10};
+    int[] B2 = {-1};
+    System.out.println(Arrays.toString(sortedSquares(B)));
+    System.out.println(Arrays.toString(sortedSquaresAlternateSolution(B1)));
+    System.out.println(Arrays.toString(sortedSquaresAlternateSolution(B2)));
   }
 
   private void executeFindNumbers() {
-    int[] nums = {12,345,2,6,7896};
+    int[] nums = {12, 345, 2, 6, 7896};
     System.out.println("executeFindNumbers: " + findNumbers(nums));
   }
 
   private void executeFindMaxConsecutiveOnes() {
-    int[] nums = {1,1,0,1,1,1,1,0,1,1,1,0,1,1,1,1,1,1,1};
-    int[] nums1 = {1,0,1,1,0,1};
+    int[] nums = {1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1};
+    int[] nums1 = {1, 0, 1, 1, 0, 1};
     System.out.println(findMaxConsecutiveOnes(nums));
   }
 
