c63b21427ff795a668d537889211dc675ae5afe9
Spike Storm <software.j2ee@gmail.com>
2020-07-12 17:41:24 -0700

    First attempt for consecutive ones, approach should be by windowing.

--- src/main/java/mx/com/geekflu/algo/prep/data/arrays/MaxConsecutiveOnesII.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/MaxConsecutiveOnesII.java
@@ -6,7 +6,33 @@ package mx.com.geekflu.algo.prep.data.arrays;
 public class MaxConsecutiveOnesII {
 
   public int findMaxConsecutiveOnes(int[] nums) {
-    return 0;
+
+    if (nums == null || nums.length == 0) return 0;
+    if (nums.length == 1) return 1;
+
+    int left = 0, right = 0;
+    int maxConsecutiveOnes = 0;
+    int onesCounter = 0;
+    boolean wasFlipped = false;
+
+    while (right < nums.length) {
+      if (nums[right] == 1) {
+        right++;
+        onesCounter++;
+        maxConsecutiveOnes = Math.max(maxConsecutiveOnes, onesCounter);
+      } else if (!wasFlipped) {
+        onesCounter++;
+        right++;
+        wasFlipped = true;
+        maxConsecutiveOnes = Math.max(maxConsecutiveOnes, onesCounter);
+      } else {
+        left = right;
+        maxConsecutiveOnes = Math.max(maxConsecutiveOnes, onesCounter);
+        wasFlipped = false;
+        onesCounter = 0;
+      }
+    }
+    return maxConsecutiveOnes;
   }
 
 }
