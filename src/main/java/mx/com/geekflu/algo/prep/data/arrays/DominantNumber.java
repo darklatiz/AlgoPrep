61961bc9922c83e3d05c3d698f8d6a49ece40f00
Spike Storm <software.j2ee@gmail.com>
2020-07-14 09:41:09 -0700

    Dominant Number

--- src/main/java/mx/com/geekflu/algo/prep/data/arrays/DominantNumber.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/DominantNumber.java
@@ -1,7 +1,7 @@
 package mx.com.geekflu.algo.prep.data.arrays;
 
 public class DominantNumber {
-  public int dominantIndex(int[] nums) {
+  public int dominantIndex(int[] A) {
     return 0;
   }
 }
