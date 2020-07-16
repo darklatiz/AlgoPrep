f62231efaaaea5063efd45cb3b34fecb475a20ce
Spike Storm <software.j2ee@gmail.com>
2020-07-16 09:38:44 -0700

    Second use case, the last use case happens when all are nines and we have to increment by one the array

--- src/main/java/mx/com/geekflu/algo/prep/data/arrays/PlusOne.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/PlusOne.java
@@ -3,19 +3,17 @@ package mx.com.geekflu.algo.prep.data.arrays;
 public class PlusOne {
 
   public int[] plusOne(int[] digits) {
-    int last = digits.length - 1;
-    if (digits[last] < 9) {
-      digits[last] += 1;
-    } else {
-      for (int sweeper = last; sweeper >= 0 && digits[sweeper] == 9; sweeper--) {
-        if (digits[sweeper] == 9) {
-          digits[sweeper] = 0;
-        }
-        if (sweeper - 1 >= 0 && digits[sweeper - 1] < 9) {
-          digits[sweeper - 1] += 1;
-        }
+    int n = digits.length;
+    for (int idx = n - 1; idx >= 0; --idx) {
+      if (digits[idx] == 9) {
+        digits[idx] = 0;
+      } else {
+        digits[idx]++;
+        return digits;
       }
     }
+    digits = new int[n + 1];
+    digits[0] = 1;
     return digits;
   }
 
