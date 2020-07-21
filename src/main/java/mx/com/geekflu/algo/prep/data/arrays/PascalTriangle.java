17a39657da1d9df4ef235e35e4e72eccb1f656b5
Spike Storm <software.j2ee@gmail.com>
2020-07-21 15:53:52 -0700

    A way to determine number of inerations, N + M -1 which is the lef down corner of the matrix

--- src/main/java/mx/com/geekflu/algo/prep/data/arrays/PascalTriangle.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/PascalTriangle.java
@@ -1,12 +1,39 @@
 package mx.com.geekflu.algo.prep.data.arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangle {
 
   public List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> triangle = new ArrayList<>();
+    if (numRows <= 0) return triangle;
 
-    return null;
+
+    List<Integer> firstRow = new ArrayList<>();
+    firstRow.add(1);
+    triangle.add(firstRow);
+
+    if (numRows == 1) return triangle;
+
+    List<Integer> secondRow = new ArrayList<>();
+    secondRow.add(1);
+    secondRow.add(1);
+    triangle.add(secondRow);
+
+    if (numRows == 2) return triangle;
+
+    for (int i = 2; i < numRows; i++) {
+      List<Integer> prev = triangle.get(i - 1);
+      List<Integer> current = new ArrayList<>();
+      current.add(1);
+      for (int j = 1; j < i; j++) {
+        current.add(prev.get(j - 1) + prev.get(j));
+      }
+      current.add(1);
+      triangle.add(current);
+    }
+    return triangle;
   }
 
 }
