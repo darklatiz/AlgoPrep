6921e6374ff52c3638a03dbd3dd967ad3d237e4b
Spike Storm <software.j2ee@gmail.com>
2020-07-21 17:21:28 -0700

    Longes common prefix

--- src/main/java/mx/com/geekflu/algo/prep/data/arrays/PascalTriangle.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/PascalTriangle.java
@@ -1,13 +1,23 @@
 package mx.com.geekflu.algo.prep.data.arrays;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PascalTriangle {
 
+  public static void main(String[] args) {
+    String[] words = {"hola", "holanes", "holanesito"};
+    PascalTriangle pt = new PascalTriangle();
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append('a');
+    stringBuilder.append('b');
+    stringBuilder.append('c');
+
+    pt.runningSum(new int[]{1, 2, 3, 4});
+    pt.longestCommonPrefix(words);
+  }
+
   public List<List<Integer>> generate(int numRows) {
-    BigInteger
     List<List<Integer>> triangle = new ArrayList<>();
     if (numRows <= 0) return triangle;
 
@@ -44,7 +54,31 @@ public class PascalTriangle {
   }
 
   public String longestCommonPrefix(String[] strs) {
-
+    if (strs == null || strs.length == 0) return "";
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int word = 0; word < strs.length; word++) {
+      char[] ws = strs[word].toCharArray();
+      char[] ws1 = strs[word + 1].toCharArray();
+      boolean match = true;
+      int index = 0;
+      while (match) {
+        if (ws.length > index && ws1.length > index && ws[index] == ws1[index]) {
+          stringBuilder.append(ws[index]);
+          index++;
+        } else {
+          match = false;
+        }
+      }
+    }
+    return "";
   }
 
+  public int[] runningSum(int[] nums) {
+    if (nums == null || nums.length == 0) return new int[1];
+
+    for (int i = 1; i < nums.length; i++) {
+      nums[i] = nums[i - 1] + nums[i];
+    }
+    return nums;
+  }
 }
