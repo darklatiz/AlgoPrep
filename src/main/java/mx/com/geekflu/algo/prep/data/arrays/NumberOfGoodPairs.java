609c48cde7902c024602b51db8c19938490c0386
Spike Storm <software.j2ee@gmail.com>
2020-07-25 14:50:16 -0700

    Longes common prefix

---
+++ src/main/java/mx/com/geekflu/algo/prep/data/arrays/NumberOfGoodPairs.java
@@ -0,0 +1,45 @@
+package mx.com.geekflu.algo.prep.data.arrays;
+
+public class NumberOfGoodPairs {
+
+  public static void main(String[] args) {
+    String[] s = {"abcd", "abcde", "abc"};
+
+    NumberOfGoodPairs numberOfGoodPairs = new NumberOfGoodPairs();
+    System.out.println(numberOfGoodPairs.longestCommonPrefix(s));
+
+  }
+
+  public int numIdenticalPairs(int[] nums) {
+
+
+    return 0;
+  }
+
+  public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0)
+      return "";
+    int minLen = Integer.MAX_VALUE;
+    for (String str : strs)
+      minLen = Math.min(minLen, str.length());
+    int low = 1;
+    int high = minLen;
+    while (low <= high) {
+      int middle = (low + high) / 2;
+      if (isCommonPrefix(strs, middle))
+        low = middle + 1;
+      else
+        high = middle - 1;
+    }
+    return strs[0].substring(0, (low + high) / 2);
+  }
+
+  private boolean isCommonPrefix(String[] strs, int len) {
+    String str1 = strs[0].substring(0, len);
+    for (int i = 1; i < strs.length; i++)
+      if (!strs[i].startsWith(str1))
+        return false;
+    return true;
+  }
+
+}
