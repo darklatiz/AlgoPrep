f54d80746adefaaf1e2952b722c7c89be4d2aa8e
Spike Storm <software.j2ee@gmail.com>
2020-07-28 20:43:21 -0700

    Linked list with getAtIndex returning the current node based on 0th index

--- src/main/java/mx/com/geekflu/algo/prep/data/linked/list/MySingleLinkedList.java
+++ src/main/java/mx/com/geekflu/algo/prep/data/linked/list/MySingleLinkedList.java
@@ -203,6 +203,20 @@ public class MySingleLinkedList {
         }
     }
 
+    public boolean hasCycle(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     public void print() {
         Node current = head;
         StringBuilder stringBuilder = new StringBuilder();
