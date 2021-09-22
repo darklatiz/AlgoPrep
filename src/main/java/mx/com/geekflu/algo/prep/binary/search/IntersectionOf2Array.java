edda2b420fe01d86b077370f855d977b729982d7
Spike Storm <software.j2ee@gmail.com>
2021-09-21 22:37:50 -0700

    Binary search algorithm is very useful

---
+++ src/main/java/mx/com/geekflu/algo/prep/binary/search/IntersectionOf2Array.java
@@ -0,0 +1,5 @@
+package mx.com.geekflu.algo.prep.binary.search;
+
+public class IntersectionOf2Array {
+
+}
---
+++ src/main/java/mx/com/geekflu/algo/prep/binary/search/Sort3Arrays.java
@@ -0,0 +1,34 @@
+package mx.com.geekflu.algo.prep.binary.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sort3Arrays {
+
+  public static List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+    List<Integer> intersection = new ArrayList<>();
+    for(var target : arr1) {
+      if (search(arr2, target) && search(arr3, target)) {
+        intersection.add(target);
+      }
+    }
+    return intersection;
+  }
+
+  private static boolean search(int[] nums, int target) {
+    int start = 0;
+    int end = nums.length - 1;
+    while(start <= end) {
+      int mid = start + (end - start) / 2;
+      if (target < nums[mid]) {
+        end = mid -1;
+      }else if (target > nums[mid]) {
+        start = mid + 1;
+      }else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
